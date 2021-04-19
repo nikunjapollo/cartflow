a95d0594c93b24d0e05fe6e38eac8cabb123f1f6
nikunj patel <nikunj.siddhiinfosoft.com>
2021-04-20 00:21:00 +0530

    Full Code

---
+++ admin-core/assets/build/editor-app.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1fe13c6c752c9a51f753be70504b14e6');
---
+++ admin-core/assets/build/settings-app.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1a6d8cab8a0033f93710a5cfb3a150b4');
